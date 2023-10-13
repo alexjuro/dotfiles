d7dd4ca06ca82906c621877641aec392224ea09a
alexjuro <alexander.lesnjak@gmail.com>
2023-10-13 09:43:20 +0200

    feat: seperate copy config file

---
+++ copyConfig.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp ./rofi/config.rasi ~/.config/rofi/config.rasi
+cp ./alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp ./alacritty/colors.yml ~/.config/alacritty/colors.yml
+cp ./picom/picom.conf ~/.config/picom/picom.conf
+cp ./qtile/autostart.sh ~/.config/qtile/autostart.sh
+cp ./qtile/config.py ~/.config/qtile/config.py
+cp ./qtile/wallpaper.png ~/.config/qtile/wallpaper.png
+cp ./starship.toml ~/.config/starship.toml
