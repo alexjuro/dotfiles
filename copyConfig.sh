397c6907cdb25a10268009b234c05887cc671424
alexjuro <alexander.lesnjak@gmail.com>
2023-10-18 00:10:12 +0200

    fix: copyConfig.sh wallpaper.png to moon.png

--- copyConfig.sh
+++ copyConfig.sh
@@ -6,5 +6,5 @@ cp ./alacritty/colors.yml ~/.config/alacritty/colors.yml
 cp ./picom/picom.conf ~/.config/picom/picom.conf
 cp ./qtile/autostart.sh ~/.config/qtile/autostart.sh
 cp ./qtile/config.py ~/.config/qtile/config.py
-cp ./qtile/wallpaper.png ~/.config/qtile/wallpaper.png
+cp ./qtile/wallpaper.png ~/.config/qtile/moon.jpg
 cp ./starship.toml ~/.config/starship.toml
