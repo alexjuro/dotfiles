97da1d1e5b08d888a97a6ab2d3fe7f26cc065399
alexjuro <alexander.lesnjak@gmail.com>
2023-10-25 13:18:47 +0200

    feat: mkdir games folder

--- retroarch/retroarch_install.sh
+++ retroarch/retroarch_install.sh
@@ -4,5 +4,7 @@ sudo pacman -S retroarch retroarch-assets-xmb retroarch-assets-ozone --noconfirm
 
 cp ./retroarch.cfg ~/.config/retroarch/retroarch.cfg
 
+mkdir ~/Documents/games
+
 #copy the retroarch.cfg
 #go to online updater and update assets
